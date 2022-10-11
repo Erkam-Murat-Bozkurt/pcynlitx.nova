a941b0476aaf1548ff65fcacbc687577108309c9
Erkam-Murat-Bozkurt <erkam.murat.bozkurt@gmail.com>
2022-10-11 14:15:59 +0300

    Executable_MakeFile_DepDeterminer class added

---
+++ MAKE.FILE.BUILD.SYSTEM/Executable_MakeFile_DepDeterminer/Executable_MakeFile_DepDeterminer.cpp
@@ -0,0 +1,241 @@
+
+
+/*
+
+Copyright ©  2021,  Erkam Murat Bozkurt
+
+This file is part of the research project which is carried by Erkam Murat Bozkurt.
+
+This is a free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation
+either version 3 of the License, or any later version.
+
+This software is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "Executable_MakeFile_DepDeterminer.hpp"
+
+Executable_MakeFile_DepDeterminer::Executable_MakeFile_DepDeterminer(){
+
+   this->Memory_Delete_Condition = false;
+
+   this->object_file_list = nullptr;
+
+   this->header_file_list = nullptr;
+
+}
+
+Executable_MakeFile_DepDeterminer::Executable_MakeFile_DepDeterminer(const
+
+          Executable_MakeFile_DepDeterminer & orig){
+
+}
+
+Executable_MakeFile_DepDeterminer::~Executable_MakeFile_DepDeterminer(){
+
+   if(!this->Memory_Delete_Condition){
+
+       this->Clear_Dynamic_Memory();
+   }
+}
+
+
+void Executable_MakeFile_DepDeterminer::Clear_Dynamic_Memory(){
+
+     if(!this->Memory_Delete_Condition){
+
+         this->Memory_Delete_Condition = true;
+     }
+}
+
+void Executable_MakeFile_DepDeterminer::Receive_Descriptor_File_Reader(Descriptor_File_Reader * Des_Reader){
+
+     this->DataCollector.Receive_Descriptor_File_Reader(Des_Reader);
+}
+
+void Executable_MakeFile_DepDeterminer::Receive_Git_Record_Data(Git_File_List_Receiver * Pointer){
+
+     this->DataCollector.Receive_Git_Record_Data(Pointer);
+}
+
+void Executable_MakeFile_DepDeterminer::Receive_Source_File_Info(Project_Files_Lister * Pointer){
+
+     this->DataCollector.Receive_Source_File_Info(Pointer);
+}
+
+void Executable_MakeFile_DepDeterminer::Determine_Dependencies(){
+
+     this->DataCollector.Collect_Make_File_Data();
+
+     this->Receive_DataCollector_Info();
+
+     this->Determine_Compile_Order();
+
+}
+
+  void Executable_MakeFile_DepDeterminer::Determine_Compile_Order(){
+
+       this->Receive_DataCollector_Info();
+
+       this->Reverse_Order_Priorities();
+
+
+       for(int i=0;i<this->header_file_number;i++){
+
+          bool rec_search = this->Data_Ptr_CString[i].rcr_srch_complated;
+
+          if(!rec_search){
+
+             this->Search_Recursive_Include_Dependency(i);
+          }
+       }
+
+
+       this->Order_Priorities();
+
+
+
+       for(int i=0;i<this->header_file_number;i++){
+
+           bool rec_search = this->Data_Ptr_CString[i].rcr_srch_complated;
+
+           this->Search_Recursive_Include_Dependency(i);
+       }
+
+       this->Order_Priorities();
+
+       this->Print_Compiler_Orders();
+  }
+
+
+  void Executable_MakeFile_DepDeterminer::Search_Recursive_Include_Dependency(int index){
+
+       int inc_num = this->Data_Ptr_CString[index].inclusion_number;
+
+       if(inc_num>0){
+
+          for(int k=0;k<inc_num;k++){
+
+              char * inc_header_name = this->Data_Ptr_CString[index].included_headers[k];
+
+              for(int j=0;j<this->header_file_number;j++){
+
+                  char * repo_header = this->Data_Ptr_CString[j].header_name;
+
+                  bool is_equal = this->Char_Processor.CompareString(inc_header_name,repo_header);
+
+                  if(is_equal){
+
+                     int priority = this->Data_Ptr_CString[j].priority;
+
+                     if(priority == 0){
+
+                        priority = 1;
+                     }
+
+                     this->Data_Ptr_CString[index].priority =
+
+                     this->Data_Ptr_CString[index].priority + priority;
+
+                  }
+              }
+          }
+        }
+        else{
+
+             this->Data_Ptr_CString[index].rcr_srch_complated = true;
+        }
+  }
+
+  void Executable_MakeFile_DepDeterminer::Order_Priorities(){
+
+       for(int i=0;i<this->header_file_number;i++){
+
+           for(int j=i;j<this->header_file_number;j++){
+
+               int dep_i = this->Data_Ptr_CString[i].priority;
+
+               int dep_j = this->Data_Ptr_CString[j].priority;
+
+               Compiler_Data_CString temp;
+
+               if( dep_i < dep_j){
+
+                   temp  = this->Data_Ptr_CString[j];
+
+                   this->Data_Ptr_CString[j] = this->Data_Ptr_CString[i];
+
+                   this->Data_Ptr_CString[i] = temp;
+               }
+            }
+        }
+  }
+
+  void Executable_MakeFile_DepDeterminer::Reverse_Order_Priorities(){
+
+       for(int i=0;i<this->header_file_number;i++){
+
+           for(int j=i;j<this->header_file_number;j++){
+
+               int dep_i = this->Data_Ptr_CString[i].priority;
+
+               int dep_j = this->Data_Ptr_CString[j].priority;
+
+               Compiler_Data_CString temp;
+
+               if( dep_i > dep_j){
+
+                   temp  = this->Data_Ptr_CString[j];
+
+                   this->Data_Ptr_CString[j] = this->Data_Ptr_CString[i];
+
+                   this->Data_Ptr_CString[i] = temp;
+               }
+
+               dep_i = this->Data_Ptr_CString[i].priority;
+
+               dep_j = this->Data_Ptr_CString[j].priority;
+            }
+        }
+  }
+
+  void Executable_MakeFile_DepDeterminer::Receive_DataCollector_Info(){
+
+       this->header_file_number = this->DataCollector.Get_Compiler_Data_Size();
+
+       this->Data_Ptr_CString   = this->DataCollector.Get_Compiler_Data();
+  }
+
+  void Executable_MakeFile_DepDeterminer::Print_Compiler_Orders(){
+
+       for(int i=0;i<this->header_file_number;i++){
+
+           std::cout << "\n\n";
+
+           std::cout << "\n --------------------------------------------------------------------------";
+
+           std::cout << "\n SOURCE FILE NUMBER:" << i;
+
+           std::cout << "\n HEADER NAME:"
+
+           <<  this->Data_Ptr_CString[i].header_name;
+
+           std::cout << "\n PRIORITY:"
+
+           <<  this->Data_Ptr_CString[i].priority;
+
+           std::cout << "\n INCLUSION NUMBER:"
+
+           <<  this->Data_Ptr_CString[i].inclusion_number;
+
+           std::cout << "\n\n";
+       }
+  }
---
+++ MAKE.FILE.BUILD.SYSTEM/Executable_MakeFile_DepDeterminer/Executable_MakeFile_DepDeterminer_Main_File.cpp
@@ -0,0 +1,62 @@
+
+
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "Executable_MakeFile_DepDeterminer.hpp"
+#include "Project_Files_Lister.h"
+#include "Git_File_List_Receiver.hpp"
+#include "Descriptor_File_Reader.hpp"
+
+int main(int argc, char ** argv){
+
+    if(argc < 2){
+
+       std::cout << "\n The usage: Make_File_Builder.exe <Descriptor File Path> ";
+
+       std::cout << "\n\n";
+
+       exit(0);
+    }
+
+    Descriptor_File_Reader Des_Reader;
+
+    Des_Reader.Read_Descriptor_File(argv[1]);
+
+    char * project_repo_dir = Des_Reader.Get_Repo_Directory_Location();
+
+    char * warehouse_path   = Des_Reader.Get_Warehouse_Location();
+
+    Git_File_List_Receiver Git_Data_Receiver;
+
+    Git_Data_Receiver.Receive_Descriptor_File_Reader(&Des_Reader);
+
+    Git_Data_Receiver.Determine_Git_Repo_Info();
+
+
+    Project_Files_Lister Dir_Lister;
+
+    Dir_Lister.Determine_Git_Repo_Info(&Des_Reader);
+
+    int src_file_num = Dir_Lister.Get_Source_File_Number();
+
+    std::cout << "\n TOTAL SOURCE FILE NUMBER:" << src_file_num;
+
+
+    Executable_MakeFile_DepDeterminer Dep_Determiner;
+
+    if(src_file_num > 0){
+
+       Dep_Determiner.Receive_Descriptor_File_Reader(&Des_Reader);
+
+       Dep_Determiner.Receive_Git_Record_Data(&Git_Data_Receiver);
+
+       Dep_Determiner.Receive_Source_File_Info(&Dir_Lister);
+
+       Dep_Determiner.Determine_Dependencies();
+
+    }
+
+    return 0;
+}
