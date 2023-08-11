2c4f50fcaac7cf47cc5e8ec77afb35450b926de4
Erkam-Murat-Bozkurt <erkam.murat.bozkurt@gmail.com>
2023-08-11 18:45:08 +0300

    Updates on script writers

--- SCRIPT.WRITERS/Project_Script_Writer/Project_Script_Writer_Main_File.cpp
+++ SCRIPT.WRITERS/Project_Script_Writer/Project_Script_Writer_Main_File.cpp
@@ -20,14 +20,39 @@ int main(int argc, char ** argv){
     }
 
 
+
+    Descriptor_File_Reader Des_File_Reader('w');    
+
+    Des_File_Reader.Receive_Descriptor_File_Path(argv[1]);
+
+    Des_File_Reader.Read_Descriptor_File();
+
+
+    Git_Data_Processor Data_Processor('w');
+
+    Data_Processor.Receive_Descriptor_File_Path(argv[1]);
+
+    Data_Processor.Write_Git_Repo_List_File();
+
+    Data_Processor.Determine_Git_Repo_Info();
+
+
     Source_File_Dependency_Determiner Dep_Determiner(argv[1],'w');
+
+    Dep_Determiner.Receive_Descriptor_File_Reader(&Des_File_Reader);
+
+    Dep_Determiner.Receive_Git_Data_Processor(&Data_Processor);
 
     Dep_Determiner.Collect_Dependency_Information();
 
 
-    Project_Script_Writer Script_Writer(argv[1],'w');
+    Project_Script_Writer Script_Writer('w');
 
     Script_Writer.Receive_Source_File_Dependency_Determiner(&Dep_Determiner);
+
+    Script_Writer.Receive_Git_Data_Processor(&Data_Processor);
+
+    Script_Writer.Receive_Descriptor_File_Reader(&Des_File_Reader);
 
     Script_Writer.Build_Compiler_Script();
 
