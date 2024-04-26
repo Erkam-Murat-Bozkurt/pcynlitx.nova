8019b7000f944504cdaf1465ea32c3a16415d06b
Erkam Murat Bozkurt <er.muratbozkurt@gmail.com>
2024-04-26 16:22:10 +0300

    Updates on GUI look and feel

--- GUI.DEVELOPMENT/ToolBar_Initializer/ToolBar_Initializer.cpp
+++ GUI.DEVELOPMENT/ToolBar_Initializer/ToolBar_Initializer.cpp
@@ -117,7 +117,7 @@ void ToolBar_Initializer::Initialize_ToolBar(wxFrame * Frame_Pointer, wxAuiDockA
 
      //this->toolBar->AddTool(ID_SHOW_HELP_MENU,wxT(""),*this->open_help_menu,wxT(""));
 
-     this->toolBar->SetMargins(3,3,4,4);
+     this->toolBar->SetMargins(0,0,0,0);
 
      this->toolBar->SetOverflowVisible(false);
 
