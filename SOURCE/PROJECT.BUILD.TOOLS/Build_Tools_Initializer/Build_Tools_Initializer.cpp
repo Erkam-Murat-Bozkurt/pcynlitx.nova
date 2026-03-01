9b48cf0605d66d95fd056544615c0ab6834abd04
Erkam Murat Bozkurt <er.muratbozkurt@gmail.com>
2026-03-01 22:31:13 +0300

    Updates on GUI components

--- SOURCE/PROJECT.BUILD.TOOLS/Build_Tools_Initializer/Build_Tools_Initializer.cpp
+++ SOURCE/PROJECT.BUILD.TOOLS/Build_Tools_Initializer/Build_Tools_Initializer.cpp
@@ -92,7 +92,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Des_Reader.Read_Descriptor_File();
 
-     char read_opr [] = "The project descriptor file read\n\n";
+     char read_opr [] = "  The project descriptor file read\n\n";
 
      std::cout << read_opr;
 
@@ -107,7 +107,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Git_Data_Proc.Determine_Git_Repo_Info();    
 
-     char git_data [] = "The data for git version controller has been collected\n\n";
+     char git_data [] = "  The data for git version controller has been collected\n\n";
 
      std::cout << git_data;
 
@@ -124,7 +124,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Dep_Determiner.Collect_Dependency_Information();
 
-     char dependency_data [] = "Source file dependencies has been determined\n\n";
+     char dependency_data [] = "  Source file dependencies has been determined\n\n";
 
      std::cout << dependency_data;
 
@@ -152,7 +152,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Rep_Init.Clear_Dynamic_Memory();
 
-     char warehouse_constration [] = "The project warehouse constructed\n\n";
+     char warehouse_constration [] = "  The project warehouse constructed\n\n";
 
      std::cout << warehouse_constration;
 
@@ -169,7 +169,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Mk_Builder.Clear_Dynamic_Memory();
      
-     char make_file_construction [] = "The project makefiles constructed\n\n";
+     char make_file_construction [] = "  The project makefiles constructed\n\n";
 
      std::cout << make_file_construction;
 
@@ -185,7 +185,7 @@ void Build_Tools_Initializer::Setup_Build_Tools(){
 
      this->Script_Writer.Build_Compiler_Script();
 
-     char script_construction [] = "The project scripts constructed\n\n";
+     char script_construction [] = "  The project scripts constructed\n\n";
 
      std::cout << script_construction;
 
